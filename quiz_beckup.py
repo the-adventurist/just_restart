c79017bc808ad359933d6bd9dcc35d9fb50947f4
the-adventurist <theface11311@gmail.com>
2024-02-13 15:24:04 +0200

    Second change

---
+++ quiz_beckup.py
@@ -0,0 +1,44 @@
+import random
+from string import ascii_lowercase
+
+NUM_QUESTIONS_PER_QUIZ = 5
+QUESTIONS = {
+    "What's the purpose of the built-in zip() function": [
+        "To iterate over two or more sequences at the same time",
+        "To combine several strings into one",
+        "To compress several files into one archive",
+        "To get information from the user",
+    ],
+    "What's the name of Python's sorting algorithm": [
+        "Timsort", "Quicksort", "Merge sort", "Bubble sort"
+    ],
+    "What does dict.get(key) return if key isn't found in dict": [
+        "None", "key", "True", "False",
+    ]
+}
+
+num_questions = min(NUM_QUESTIONS_PER_QUIZ, len(QUESTIONS))
+questions = random.sample(list(QUESTIONS.items()), k=num_questions)
+
+num_correct = 0
+for num, (question, alternatives) in enumerate(questions, start=1):
+    print(f"\nQuestion {num}:")
+    print(f"{question}?")
+    correct_answer = alternatives[0]
+    labeled_alternatives = dict(
+        zip(ascii_lowercase, random.sample(alternatives, k=len(alternatives)))
+    )
+    for label, alternative in labeled_alternatives.items():
+        print(f"  {label}) {alternative}")
+
+    while (answer_label := input("\nChoice? ")) not in labeled_alternatives:
+        print(f"Please answer one of {', '.join(labeled_alternatives)}")
+
+    answer = labeled_alternatives[answer_label]
+    if answer == correct_answer:
+        num_correct += 1
+        print("⭐ Correct! ⭐")
+    else:
+        print(f"The answer is {correct_answer!r}, not {answer!r}")
+
+print(f"\nYou got {num_correct} correct out of {num} questions")
