c73d6a2591217890731c1da9b8ffd9ee6a1673da
the-adventurist <theface11311@gmail.com>
2024-02-14 16:07:48 +0200

    final quiz project

--- quiz.py
+++ quiz.py
@@ -21,3 +21,24 @@ QUESTIONS = {
 def prepare_questions(questions, num_questions):
     num_questions = min(num_questions, len(questions))
     return random.sample(list(questions.items()), k=num_questions)
+
+
+def get_answer(question, alternatives):
+    print(f"{question}?")
+    labeled_alternatives = dict(zip(ascii_lowercase, alternatives))
+    for label, alternative in labeled_alternatives.items():
+        print(f"   {label}) alternative")
+
+    while (answer_label := input("\nChoice?")) not in labeled_alternatives:
+        print(f"Please answer one of {', '.join(labeled_alternatives)}")
+
+    return labeled_alternatives
+
+
+def ask_question(question, alternatives):
+    correct_answer = alternatives[0]
+    ordered_alternatives = random.sample(alternatives, k=len(alternatives))
+
+    answer = get_answer(question, ordered_alternatives)
+    if answer == correct_answer:
+        print()
